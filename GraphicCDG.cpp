c8b7ec315d44b77351e1119d9b8d647b3f87be6d
n-nagar <nagar@yahoo.com>
2016-02-25 09:50:39 -0800

    Converted to GLFW

--- GraphicCDG.cpp
+++ GraphicCDG.cpp
@@ -1,35 +1,57 @@
 #include "CDGParser.h"
-#include <GL/gl.h>
-#include <GL/glut.h>
-#include <GL/glu.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstring>
 #include <cstdio>
 
-GLushort screen[CDGScreenHandler::HEIGHT][CDGScreenHandler::WIDTH];
+GLushort *screen_buffer;
 
-void RefreshScreen(void)
+void *RefreshScreen(GLFWwindow *win)
 {
 	glDrawPixels(CDGScreenHandler::WIDTH, CDGScreenHandler::HEIGHT, GL_RGBA, 
-                GL_UNSIGNED_SHORT_4_4_4_4, screen);
-	glFlush();
+                GL_UNSIGNED_SHORT_4_4_4_4, screen_buffer);
+	//glFlush();
+	glfwSwapBuffers(win);
+}
+
+void ResizeScreen(GLFWwindow *window, int width, int height)
+{
+	//fprintf(stderr, "Resize[%d][%d]\n", height, width );
+  	glViewport(0, 0, width, height);
 }
 
 class GraphicsDisplay : public CDGScreenHandler
 {
 private:
+	char *song_path;
+	GLFWwindow *window;
 	unsigned short screen_colors[MAX_COLORS];
 
 public:
 	GraphicsDisplay(int *argc, char **argv)
 	{
-		glutInit(argc, argv);
-		glutInitWindowSize(WIDTH, HEIGHT);
-		glutDisplayFunc(RefreshScreen);
-		glutIdleFunc(RefreshScreen);
-		glutCreateWindow(argv[1]);
+		window = NULL;
+		screen_buffer = NULL;
+		if (*argc < 2)
+			return;
+		song_path = new char[strlen(argv[1])+1];
+		strcpy(song_path, argv[1]);
+		if (!glfwInit())
+			return;
+		if ((window = glfwCreateWindow(WIDTH, HEIGHT, argv[1], NULL, NULL)) == NULL)
+			return;
+		glfwSetWindowRefreshCallback(window, (GLFWwindowrefreshfun)RefreshScreen);
+		glfwSetWindowSizeCallback(window, (GLFWwindowsizefun)ResizeScreen);
+		screen_buffer = new GLushort[HEIGHT * WIDTH];
+		glfwMakeContextCurrent(window);
 	}
 
 	~GraphicsDisplay()
 	{
+		if (screen_buffer)
+			delete[] screen_buffer;
+		if (window)
+			glfwTerminate();
 	}
 
 	void InitColors(const unsigned short colors[])
@@ -40,27 +62,47 @@ public:
 
 	void Display(const Screen *s)
 	{
+		if (!screen_buffer)
+			return;
 		for (int i = 0; i < HEIGHT; i++)
 		{
 			int gl_y = HEIGHT - i - 1;
 			for (int j = 0; j < WIDTH; j++)
-				screen[gl_y][j] = screen_colors[(*s)[i][j]];
+				screen_buffer[gl_y * WIDTH + j] = screen_colors[(*s)[i][j]];
 		}
-		//glutPostRedisplay();
+		glfwPostEmptyEvent();
+	}
+
+	void MainLoop()
+	{		
+		CDGParser *c;
+
+		if (!window)
+			return;
+		c = CDGParser::GetParser(song_path, this);
+		c->Start();
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwWaitEvents();
+			RefreshScreen(window);
+		}
+		delete c;
 	}
 
 };
 
 int main(int argc, char *argv[])	
 {
-	CDGParser *c;
-
 	if (argc > 1)
 	{
-		c = CDGParser::GetParser(argv[1], new GraphicsDisplay(&argc, argv));
-		c->Start();
-		glutMainLoop();
-		delete c;
+		GraphicsDisplay *gd = new GraphicsDisplay(&argc, argv);
+		if (gd == NULL)
+		{
+			std::cerr << "Cannot create GraphicsDisplay\n";
+			return -1;
+		}
+		gd->MainLoop();
+		delete gd;
 	}
 	return 0;
 }
